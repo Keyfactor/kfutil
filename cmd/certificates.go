41934879617523455e15ec32687522da5af48dd8
sbailey <1661003+spbsoluble@users.noreply.github.com>
2022-11-17 12:30:13 -0800

    fix(cmd): Remove `certificates` sub CLI as it's not ready.

--- cmd/certificates.go
+++ cmd/certificates.go
@@ -15,20 +15,15 @@ import (
 // certificatesCmd represents the certificates command
 var certificatesCmd = &cobra.Command{
 	Use:   "certificates",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Keyfactor Command certificate APIs and utilities.",
+	Long:  `A collections of APIs and utilities for interacting with Keyfactor certificates.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("certificates called")
 	},
 }
 
 func init() {
-	RootCmd.AddCommand(certificatesCmd)
+	//RootCmd.AddCommand(certificatesCmd)
 
 	// Here you will define your flags and configuration settings.
 
