a770d9fb935902027f7859ab32522adbf660863b
Sean <1661003+spbsoluble@users.noreply.github.com>
2023-01-26 07:20:43 -0800

    fix(script): Install script full cleanup and exported variable scope fix.

--- gh-dl-release.sh
+++ gh-dl-release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #gh auth login
+set -e
 export REPO_NAMESPACE=Keyfactor
 export REPO_NAME=kfutil
 export REPO_PATH="${REPO_NAMESPACE}/${REPO_NAME}"
@@ -7,7 +8,6 @@ export TAG=$(gh release list --repo $REPO_PATH --limit 1 | cut -f 1)
 export RELEASE_DIR="${REPO_NAME}-${TAG}"
 export OS_ARCH=$(uname -m)
 export OS_NAME=$(uname -s)
-export FILE_PATTERN=$(echo "${OS_NAME}_${OS_ARCH}" | tr '[:upper:]' '[:lower:]')
 
 function check_installed_binary() {
   bin_installed=$(which "$1")
@@ -19,12 +19,14 @@ function check_installed_binary() {
 
 # convert to amd64
 if [[ "$OS_ARCH" == "x86_64" ]]; then
-  OS_ARCH="amd64"
+  export OS_ARCH="amd64"
 fi
 # convert to arm64
 if [[ "$OS_ARCH" == "aarch64" ]]; then
-  OS_ARCH="arm64"
+  export OS_ARCH="arm64"
 fi
+
+export FILE_PATTERN=$(echo "${OS_NAME}_${OS_ARCH}" | tr '[:upper:]' '[:lower:]')
 
 # check deps
 check_installed_binary "gh"
@@ -32,9 +34,8 @@ check_installed_binary "zip"
 check_installed_binary "unzip"
 
 # download release
-mkdir -p "$RELEASE_DIR" && \
-  cd "$RELEASE_DIR" && \
-  gh release download "$TAG" --repo $REPO_PATH --pattern "*${FILE_PATTERN}*" --clobber
+mkdir -p "$RELEASE_DIR" || true
+cd "$RELEASE_DIR" && gh release download "$TAG" --repo $REPO_PATH --pattern "*${FILE_PATTERN}*" --clobber
 
 # unzip release
 cd .. && zip -r "${RELEASE_DIR}.zip" "$RELEASE_DIR"
@@ -45,7 +46,7 @@ mkdir -p "${HOME}/.local/bin/" && \
   mv "${REPO_NAME}" "${HOME}/.local/bin/${REPO_NAME}"
 
 # cleanup
-cd .. && rm -rf "${RELEASE_DIR}"
+cd .. && rm -rf "${RELEASE_DIR}" && rm -f "${REPO_NAME}-v${TAG}.zip"
 
 # test
 check_installed_binary "${REPO_NAME}"
