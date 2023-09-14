03c20059aff1eb271223ab324367fb5bd10efe8f
sbailey <1661003+spbsoluble@users.noreply.github.com>
2023-09-14 11:48:34 -0700

    fix(script): use passed vars for akv auth

--- examples/auth/akv/akv_auth.sh
+++ examples/auth/akv/akv_auth.sh
@@ -6,34 +6,37 @@ default_vault_name="${VAULT_NAME:-kfutil}"
 default_secret_name="${SECRET_NAME:-integration-labs}"
 
 export METADATA_URL="http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net"
-echo "Metadata URL: $METADATA_URL"
+
 
 read_keyvault_secret() {
+  local vault_name="$1"
+  local secret_name="$2"
   # Make a request to the metadata endpoint
   echo "Querying metadata endpoint for access token..."
-  TOKEN_JSON=$(curl -H "Metadata: true" $METADATA_URL)
+  echo "Metadata URL: $METADATA_URL"
+  token_json=$(curl -H "Metadata: true" $METADATA_URL)
 
-  echo "Exporting access token to ACCESS_TOKEN variable..."
+  echo "Exporting access token to access_token variable..."
   # Parse the access token from the response JSON
-  export ACCESS_TOKEN=$(echo $TOKEN_JSON | jq -r .access_token)
+  access_token=$(echo $token_json | jq -r .access_token)
 
-  # Now you can use the $ACCESS_TOKEN to authenticate and access Azure Key Vault
-  echo "Access Token: $ACCESS_TOKEN"
+  # Now you can use the $access_token to authenticate and access Azure Key Vault
+  echo "Access Token: $access_token"
 
-  export SECRET_URL="https://${VAULT_NAME}.vault.azure.net/secrets/${SECRET_NAME}?api-version=7.0"
-  echo "Secret URL: $SECRET_URL"
+  secret_url="https://${vault_name}.vault.azure.net/secrets/${secret_name}?api-version=7.0"
+  echo "Secret URL: $secret_url"
 
   # Create a new secret in Azure Key Vault
-  #SECRET_VALUE="meow"
-  #curl -X PUT -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "Content-Type: application/json" -d "{\"value\": \"${SECRET_VALUE}\"}" "$SECRET_URL"
+  #secret_value="meow"
+  #curl -X PUT -H "Authorization: Bearer ${access_token}" -H "Content-Type: application/json" -d "{\"value\": \"${secret_value}\"}" "$secret_url"
 
   # Get the secret value from Azure Key Vault
   echo "Querying Azure Key Vault for secret value..."
-  SECRET_VALUE=$(curl -H "Authorization: Bearer ${ACCESS_TOKEN}" "$SECRET_URL" | jq -r .value)
+  secret_value=$(curl -H "Authorization: Bearer ${access_token}" "$secret_url" | jq -r .value)
 
-  #echo "Secret Value: $SECRET_VALUE"
+  #echo "Secret Value: $secret_value"
   mkdir -p ~/.keyfactor
-  echo $SECRET_VALUE | jq -r . > ~/.keyfactor/command_config.json
+  echo $secret_value | jq -r . > ~/.keyfactor/command_config.json
 }
 
 # Main script logic
