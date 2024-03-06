05f4f31fea459e9a7eae94a49a0d41c5d3cd8d83
sbailey <1661003+spbsoluble@users.noreply.github.com>
2024-03-06 13:04:05 -0800

    chore(pkg): Bump license year
    Signed-off-by: sbailey <1661003+spbsoluble@users.noreply.github.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,4 +1,4 @@
-// Package version Copyright 2023 Keyfactor
+// Package version Copyright 2024 Keyfactor
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
