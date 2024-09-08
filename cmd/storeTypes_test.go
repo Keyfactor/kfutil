b8162b65b9957b0768411120118d4d8c2e1b430f
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2024-09-08 12:24:59 -0700

    fix(tests): format gitref
    Signed-off-by: spbsoluble <1661003+spbsoluble@users.noreply.github.com>

--- cmd/storeTypes_test.go
+++ cmd/storeTypes_test.go
@@ -164,7 +164,7 @@ func Test_StoreTypesCreateFromTemplatesCmd(t *testing.T) {
 		ghBranch = strings.Replace(ghBranch, "refs/heads/", "", 1)
 		// url escape the branch name
 		ghBranch = url.QueryEscape(ghBranch)
-		testArgs = append(testArgs, "--git-ref", fmt.Sprintf("'%s'", ghBranch))
+		testArgs = append(testArgs, "--git-ref", fmt.Sprintf("%s", ghBranch))
 		t.Log("GITHUB_REF: ", ghBranch)
 	}
 	t.Log("testArgs: ", testArgs)
