12cffde2be8a2ba94bdd5da8fc4afee98894e6ae
spbsoluble <1661003+spbsoluble@users.noreply.github.com>
2024-11-01 12:38:28 -0700

    fix(ci): Correct env config for 1050_CLEAN
    Signed-off-by: spbsoluble <1661003+spbsoluble@users.noreply.github.com>

--- .github/config/environments.tf
+++ .github/config/environments.tf
@@ -14,7 +14,7 @@ module "keyfactor_github_test_environment_10_5_0_CLEAN" {
 
   gh_environment_name = "KFC_10_5_0_CLEAN"
   gh_repo_name        = data.github_repository.repo.name
-  keyfactor_hostname  = var.keyfactor_hostname_10_5_0
+  keyfactor_hostname = var.keyfactor_hostname_10_5_0_CLEAN
   keyfactor_username  = var.keyfactor_username_AD
   keyfactor_password  = var.keyfactor_password_AD
   keyfactor_config_file = base64encode(file("${path.module}/command_config.json"))
@@ -36,7 +36,7 @@ module "keyfactor_github_test_environment_11_5_0_CLEAN" {
 
   gh_environment_name = "KFC_11_5_0_CLEAN"
   gh_repo_name        = data.github_repository.repo.name
-  keyfactor_hostname  = var.keyfactor_hostname_11_5_0
+  keyfactor_hostname = var.keyfactor_hostname_11_5_0_CLEAN
   keyfactor_username  = var.keyfactor_username_AD
   keyfactor_password  = var.keyfactor_password_AD
   keyfactor_config_file = base64encode(file("${path.module}/command_config.json"))
@@ -68,22 +68,22 @@ module "keyfactor_github_test_environment_11_5_0_OAUTH_CLEAN" {
   keyfactor_config_file = base64encode(file("${path.module}/command_config.json"))
 }
 
-module "keyfactor_github_test_environment_12_3_0_AD_CLEAN" {
+module "keyfactor_github_test_environment_12_3_0_AD" {
   source                    = "git::ssh://git@github.com/Keyfactor/terraform-module-keyfactor-github-test-environment-ad.git?ref=main"
-  gh_environment_name       = "KFC_12_3_0_AD_CLEAN"
+  gh_environment_name       = "KFC_12_3_0_AD"
   gh_repo_name              = data.github_repository.repo.name
-  keyfactor_hostname        = var.keyfactor_hostname_12_3_0_CLEAN
+  keyfactor_hostname        = var.keyfactor_hostname_12_3_0
   keyfactor_username        = var.keyfactor_username_AD
   keyfactor_password        = var.keyfactor_password_AD
   keyfactor_tls_skip_verify = true
   keyfactor_config_file = base64encode(file("${path.module}/command_config.json"))
 }
 
-module "keyfactor_github_test_environment_12_3_0_AD" {
+module "keyfactor_github_test_environment_12_3_0_AD_CLEAN" {
   source                    = "git::ssh://git@github.com/Keyfactor/terraform-module-keyfactor-github-test-environment-ad.git?ref=main"
-  gh_environment_name       = "KFC_12_3_0_AD"
+  gh_environment_name       = "KFC_12_3_0_AD_CLEAN"
   gh_repo_name              = data.github_repository.repo.name
-  keyfactor_hostname        = var.keyfactor_hostname_12_3_0
+  keyfactor_hostname        = var.keyfactor_hostname_12_3_0_CLEAN
   keyfactor_username        = var.keyfactor_username_AD
   keyfactor_password        = var.keyfactor_password_AD
   keyfactor_tls_skip_verify = true
